4c54174fb09cf420087c3a89df0d619ce41616b3
Alexis Grimaldi <sopo.dev@gmail.com>
2009-11-10 19:07:53 +0100

    Utilities library, for various function not necessarilly related to the main code.

---
+++ Utils.c
@@ -0,0 +1,16 @@
+/*
+ *  Utils.c
+ *  SeqAlign
+ *
+ *  Created by sopo on 09/11/09.
+ *  Copyright 2009 UPMC. All rights reserved.
+ *
+ */
+
+#include "Utils.h"
+
+
+int intArrayLength( int *array )
+{
+	return sizeof( array ) / sizeof( int );
+}
---
+++ Utils.h
@@ -0,0 +1,10 @@
+/*
+ *  Utils.h
+ *  SeqAlign
+ *
+ *  Created by sopo on 09/11/09.
+ *  Copyright 2009 UPMC. All rights reserved.
+ *
+ */
+
+int intArrayLength( int *array );
