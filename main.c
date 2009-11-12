e1bd1b48b772b4aae2dba5c89645780f0fc7619b
Alexis Grimaldi <sopo.dev@gmail.com>
2009-11-12 23:48:54 +0100

    Very basic command line options.
    - No option or 'consistency' will build a small
    	tree from a static sequence and perform a
    	search on it.
    
    - 'performance' will build a tree of depth 20
    	from a random sequence 1,000,000 characters
    	long

--- main.c
+++ main.c
@@ -11,33 +11,75 @@
 
 int main (int argc, const char * argv[]) {
 	
+	int task;
+	int depth;
+	int *readLengths;
+	char *s;
+	Sequence *ss;
+	
 /*
 tggatggagaggtaaatgcctacgcgatttgcgtgatagagcggatgcgcgttgagtttgtcgttaaggatgggattaaaacggtcaggactgtttgat
 */
-//	char *s = "tggatggagaggtaaatgcctacgcgatttgcgtgatagagcggatgcgcgttgagtttgtcgttaaggatgggattaaaacggtcaggactgtttgat";
-	Sequence *s = New_Sequence( NULL );
+
+	if (argc > 3)
+	{
+		return ( -1 );
+	}
+	else if ( argc == 1 || ( argc == 2 && strcmp( argv[1], "consistency" ) == 0 ) )
+	{
+		task = 1;
+	}
+	else if ( argc == 2 && strcmp( argv[1], "performance" ) == 0 )
+	{
+		task = 2;
+	}
 	
-	s->sequence = s->Random( s, 2000000 );
-	
-	printf( "sequence genereated\n" );
+	if (task == 1)
+	{
+		s = "tggatggagaggtaaatgcctacgcgatttgcgtgatagagcggatgcgcgttgagtttgtcgttaaggatgggattaaaacggtcaggactgtttgat";
+		
+		int tmp_readL[] = { 3, 4 };
+		readLengths = tmp_readL;
+		
+		depth = 4;
+	}
+	else
+	{
+		ss = New_Sequence( NULL );
+		
+		ss->sequence = Sequence_Random( ss, 1000000 );
+		
+		s = ss->sequence;
+		
+		printf( "sequence genereated\n" );
+		
+		depth = 20;
+	}
+
 	
 	Node *root = New_Node( '0' );
 	
 	printf( "Size of a NODE : %ld\n", sizeof( *root ) );
 	
-	int readLengths[] = { 18, 19, 20 };
 	
-	Node_Build( root, s->sequence, 20, readLengths );
+	Node_Build( root, s, depth, readLengths );
 	
 	printf("Tree built\n");
 	
-//	Node_PrintIndexes( Node_GetReadIndexes( root, "ttt" ) );
+	
+	if (task == 1)
+	{
+		Node_PrintIndexes( Node_GetReadIndexes( root, "ttt" ) );
+	}
 
 //	getchar();
 	
 	Node_Free( root );	
-	
-	s->Free( s );
+
+	if (task == 2)
+	{
+		Sequence_Free( ss );
+	}
 	
     return 0;
 }
