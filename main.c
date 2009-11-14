9c71f0da832287b933b6f0363f20944cada28e43
Alexis Grimaldi <sopo.dev@gmail.com>
2009-11-14 15:59:26 +0100

    New option for the main.
    'performance speed' will print the CPU time and free everything.
    
    'performance memory' will prompt the user so that he has the time to check
    how much memory the tree has eaten.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "Utils.h"
 #include "Sequence.h"
@@ -10,6 +11,9 @@
 
 
 int main (int argc, const char * argv[]) {
+	
+	clock_t start, end;
+	double elapsed;
 	
 	int task;
 	int depth;
@@ -21,7 +25,7 @@ int main (int argc, const char * argv[]) {
 tggatggagaggtaaatgcctacgcgatttgcgtgatagagcggatgcgcgttgagtttgtcgttaaggatgggattaaaacggtcaggactgtttgat
 */
 
-	if (argc > 3)
+	if (argc > 4)
 	{
 		return ( -1 );
 	}
@@ -29,10 +33,18 @@ tggatggagaggtaaatgcctacgcgatttgcgtgatagagcggatgcgcgttgagtttgtcgttaaggatgggattaaa
 	{
 		task = 1;
 	}
-	else if ( argc == 2 && strcmp( argv[1], "performance" ) == 0 )
+	else if ( strcmp( argv[1], "performance" ) == 0 )
 	{
-		task = 2;
+		if ( argc == 2 || ( argc == 3 && strcmp( argv[ 2 ], "speed" ) == 0 ) )
+		{
+			task = 3;
+		}
+		else if ( argc == 3 && strcmp( argv[ 2 ], "memory" ) == 0 )
+		{
+			task = 2;
+		}
 	}
+	
 	
 	if (task == 1)
 	{
@@ -43,6 +55,8 @@ tggatggagaggtaaatgcctacgcgatttgcgtgatagagcggatgcgcgttgagtttgtcgttaaggatgggattaaa
 		
 		depth = 4;
 	}
+	
+	
 	else
 	{
 		ss = New_Sequence( NULL );
@@ -53,30 +67,56 @@ tggatggagaggtaaatgcctacgcgatttgcgtgatagagcggatgcgcgttgagtttgtcgttaaggatgggattaaa
 		
 		printf( "sequence genereated\n" );
 		
+		int tmp_readL[] = { 18, 19, 20 };
+		readLengths = tmp_readL;
+		
 		depth = 20;
 	}
-
+	
+	
+	
+	
 	
 	Node *root = New_Node( '0' );
 	
 	printf( "Size of a NODE : %ld\n", sizeof( *root ) );
 	
+	if (task == 3)
+		start = clock();
 	
 	Node_Build( root, s, depth, readLengths );
 	
+	if (task == 3)
+		end = clock();
+	
 	printf("Tree built\n");
+	
+	
+	
+	
 	
 	
 	if (task == 1)
 	{
 		Node_PrintIndexes( Node_GetReadIndexes( root, "ttt" ) );
+		printf("\n");
+		Node_PrintIndexes( Node_GetReadIndexes( root, "ttga" ) );
 	}
-
-//	getchar();
+	
+	if (task == 3)
+	{
+		elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
+		printf( "TIME : %f", elapsed );
+	}
+	printf("%i\n", task);
+	if (task == 2)
+	{
+		getchar();
+	}
 	
 	Node_Free( root );	
 
-	if (task == 2)
+	if (task == 2 || task == 3 )
 	{
 		Sequence_Free( ss );
 	}
